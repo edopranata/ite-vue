62fd6703af7180247725e45f48f48d8c0fb2bc91
edopranata <edo.pranata@gmail.com>
2023-12-06 09:24:42 +0700

    Change admin layout

--- src/layouts/admin/Admin.vue
+++ src/layouts/admin/Admin.vue
@@ -43,32 +43,54 @@
         app
       >
         <v-app-bar-nav-icon @click.stop="appStore.toggleDrawer()" />
-        <v-btn
-          icon
-          @click.stop="appStore.toggleRail()"
-        >
-          <v-icon>mdi-{{ `chevron-${appStore.theme.rail ? 'right' : 'left'}` }}</v-icon>
-        </v-btn>
-        <v-btn
-          icon
-          @click.stop="appStore.toggleOrder()"
-        >
-          <v-icon>mdi-application</v-icon>
-        </v-btn>
-        <v-btn
-          icon
-          @click.stop="appStore.toggleFixed()"
-        >
-          <v-icon>mdi-minus</v-icon>
-        </v-btn>
+        <v-tooltip text="Toggle mini sidebar">
+          <template v-slot:activator="{ props }">
+            <v-btn
+              v-bind="props"
+              icon
+              @click.stop="appStore.toggleRail()"
+            >
+              <v-icon>mdi-{{ `chevron-${appStore.theme.rail ? 'right' : 'left'}` }}</v-icon>
+            </v-btn>
+          </template>
+        </v-tooltip>
 
+        <v-tooltip text="Toggle fluid navbar">
+          <template v-slot:activator="{ props }">
+            <v-btn
+              v-bind="props"
+              icon
+              @click.stop="appStore.toggleOrder()"
+            >
+              <v-icon>mdi-application</v-icon>
+            </v-btn>
+          </template>
+        </v-tooltip>
+
+        <v-tooltip text="Toggle fixed layout">
+          <template v-slot:activator="{ props }">
+            <v-btn
+              v-bind="props"
+              icon
+              @click.stop="appStore.toggleFixed()"
+            >
+              <v-icon>mdi-minus</v-icon>
+            </v-btn>
+          </template>
+        </v-tooltip>
         <v-spacer />
-        <v-btn
-          icon
-          @click.stop="appStore.toggleTheme()"
-        >
-          <v-icon>mdi-theme-light-dark</v-icon>
-        </v-btn>
+        <v-tooltip text="Toggle dark mode">
+          <template v-slot:activator="{ props }">
+            <v-btn
+              v-bind="props"
+              icon
+              @click.stop="appStore.toggleTheme()"
+            >
+              <v-icon>mdi-theme-light-dark</v-icon>
+            </v-btn>
+          </template>
+        </v-tooltip>
+
       </v-app-bar>
       <v-main>
         <v-container>
